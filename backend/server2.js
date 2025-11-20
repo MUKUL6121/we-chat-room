17c1b6e2f7ad14b773396eb16882ea645783522c
Mukul Jangra <148455498+MUKUL6121@users.noreply.github.com>
2025-11-20 06:25:44 +0530

    Update server2.js

--- backend/server2.js
+++ backend/server2.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://test-room-site.onrender.com"] // your React app
+    origin: ["http://localhost:5173", "https://test-room-site.onrender.com"], // your React app
     methods: ["GET", "POST"],
   },
 });
